5b4e298872c6709f095e5ab7df2b5fe89f03f55e
tm1981 <tamer.mansour@gmail.com>
2021-08-28 14:47:19 +0300

    update server file

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -27,6 +27,26 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use("/api/v0/", IndexRouter);
 
+  app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    //res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  
+    // Pass to next layer of middleware
+    next();
+  });
+
   // Root URI call
   app.get("/", async (req, res) => {
     res.send("/api/v0/");
